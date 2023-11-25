b1528175904eaa4c326f9faa629ccf69fb249b2c
TeluguZone <140744388+TeluguZone@users.noreply.github.com>
2023-11-26 00:07:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,11 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/1238muj/THALAPATHY-FILTER-BOT.git /THALAPATHY-FILTER-BOT 
-else
+  git clone https://github.com/TeluguZone/Daemon88
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /THALAPATHY-FILTER-BOT 
+  git clone $UPSTREAM_REPO /TELUGUZONE-FILTER-BOT 
 fi
-cd /THALAPATHY-FILTER-BOT 
+cd /TELUGUZONE-FILTER-BOT 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
